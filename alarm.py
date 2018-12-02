b55fdf4bd0005c4c7b2e0b0785329ed996075d7f
Pranav Bhusari <pranavbhusari2013@gmail.com>
2018-12-02 12:21:39 -0500

    call proc

--- alarm.py
+++ alarm.py
@@ -4,9 +4,11 @@ import time
 #plays the sound
 subprocess.call(["mpg123", "--loop", "10", "./alarm.mp3"])
 
-time.sleep(10)
+print("deb1")
 
 #stops the alarm
 subprocess.call(["killall", "mpg123"])
+
+print("deb2")
 
 
