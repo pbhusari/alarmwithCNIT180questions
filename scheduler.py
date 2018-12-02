b2f5c097c09851d8eb74169aa05a7ba99d7d2a0e
Pranav Bhusari <pranavbhusari2013@gmail.com>
2018-12-02 13:34:09 -0500

    more questions and commas on lists

--- scheduler.py
+++ scheduler.py
@@ -6,15 +6,15 @@ from datetime import time
 print("Requesting from Google Calendar ")
 print("Unpacking from Google Calendar")
 
-file_cron = CronTab(tabfile='testtab.tab')
-mem_cron = CronTab(tab="""
-  * * * * * command
-""")
-
-job = file_cron.new(command='/home/pranav/piproject/alarmwithCNIT180questions/alarm.py')
-
-job.setall(time(8, 30))
-
+# file_cron = CronTab(tabfile='testtab.tab')
+# mem_cron = CronTab(tab="""
+#   * * * * * command
+# """)
+#
+# job = file_cron.new(command='/home/pranav/piproject/alarmwithCNIT180questions/alarm.py')
+#
+# job.setall(time(8, 30))
+#
 
 print("making cron job")
 
