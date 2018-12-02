0a4df1c44345cce458ff413f723e596d5367a7bb
Pranav Bhusari <pranavbhusari2013@gmail.com>
2018-12-02 11:59:41 -0500

    Initial commit

---
+++ scheduler.py
@@ -0,0 +1,17 @@
+from crontab import CronTab
+from datetime import time
+
+file_cron = CronTab(tabfile='testtab.tab')
+mem_cron = CronTab(tab="""
+  * * * * * command
+""")
+
+job = file_cron.new(command='/home/pranav/piproject/alarm.py')
+
+job.setall(time(8, 30))
+
+
+print("Requesting from Google Calendar ")
+print("Unpacking from Google Calendar")
+print("making chron job")
+
